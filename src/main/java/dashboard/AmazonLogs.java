6d7645d18631d01fa5421511a328627ddf6e7f98
cl-server <cl-server@ip-10-88-185-82.ec2.internal>
2013-05-02 15:22:06 +0000

    init

---
+++ src/main/java/dashboard/AmazonLogs.java
@@ -0,0 +1,145 @@
+/*
+ * Copyright (c) 2013 Dror. All rights reserved
+ * <p/>
+ * The software source code is proprietary and confidential information of Dror.
+ * You may use the software source code solely under the terms and limitations of
+ * the license agreement granted to you by Dror.
+ */
+
+package dashboard;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+
+import java.util.zip.GZIPInputStream;
+
+// Amazon
+import com.amazonaws.auth.AWSCredentials;
+import com.amazonaws.auth.BasicAWSCredentials;
+import com.amazonaws.services.s3.AmazonS3Client;
+import com.amazonaws.services.s3.model.*;
+
+
+public class AmazonLogs
+{
+
+   // ======================================  
+   // Read Amazon CloudFront Logs (Zip files) 
+   // 
+   // ======================================
+   public int readAmazonLogs(int n, 
+                             String AWS_USER, String AWS_PASS, String bucketName, String DELETE_PROCESSED_LOGS,
+                             String API_KEY, String TOKEN,
+                             String apiuser, String apipass) throws Exception {
+                             
+        if (n < 1) return 0;  
+        int eventsNumber = 0;
+        String line = null;
+        int begin = 0;
+        int zips = 0;
+        int deletedZips = 0;
+        int mixpanelStatus = 0;
+        String registrant = "";
+        
+        Mixpanel mix = new Mixpanel();
+        //Whois w = new Whois();
+        
+        // Log files Bucket
+        AWSCredentials credentials = new BasicAWSCredentials(AWS_USER,AWS_PASS);
+        AmazonS3Client s3Client = new AmazonS3Client(credentials);
+        ListObjectsRequest listObjectsRequest = new ListObjectsRequest().withBucketName(bucketName);
+        
+        // Set MARKER - from which Log File to start reading
+        // listObjectsRequest.setMarker("E2DXXJR0N8BXOK.2013-03-18-10.ICK6IvaY.gz");
+        
+        BufferedReader br = null;
+        
+        ObjectListing objectListing = s3Client.listObjects(listObjectsRequest);
+        ObjectListing nextObjectListing = objectListing;
+        zips = 0;
+        Boolean more = true;
+        if (objectListing == null) more = false;
+                
+        while (more) {
+        // Reads 1000 files
+        for (S3ObjectSummary objectSummary : objectListing.getObjectSummaries()) {
+          // Handle  ZIP files        
+          
+          try { // Open and send to mixpanel events of one ZIP file  
+            String key = objectSummary.getKey();
+         
+            S3Object object = s3Client.getObject(new GetObjectRequest(bucketName, key));
+            // Extract ZIP and read Object to reader
+            br = new BufferedReader(new InputStreamReader(new GZIPInputStream(object.getObjectContent())));
+            zips++;      
+            
+            // Read the lines from the unzipped file, break it and send to Mixpanel
+            while ((line = br.readLine()) != null) {
+                if (line.startsWith("#")) continue;
+                if (line.trim().equals("")) continue;
+                String[] values = line.split("\\s");  
+                
+                String eventTime = values[0] + " " + values[1];
+                String ip = values[4];
+                
+                // WHOIS - Check registrant of this IP address
+                //registrant = w.whois( ip, apiuser, apipass ); 
+                
+                String method = values[5];
+                String fileName = values[7];
+                String statusCode = values[8];
+                String userAgent = values[10];
+                String fName = fileName;
+                
+                if (fileName.contains("gigaspaces-")) {
+                   begin = fileName.lastIndexOf("gigaspaces-") + 11;
+                   fName = fileName.substring(begin, fileName.length()); 
+                }
+
+                eventsNumber++; 
+                System.out.println(eventsNumber + ": " + eventTime + " " + ip );
+
+                // ====================================================
+                // Track the event in Mixpanel (using the POST import)
+                // ====================================================
+                mixpanelStatus = mix.postCDNEventToMixpanel(API_KEY, TOKEN, ip, "Cloudfront CDN", eventTime, method,  fileName, fName, userAgent, statusCode, registrant);
+      
+            } // while on ZIP file lines
+     
+            if (mixpanelStatus == 1 & DELETE_PROCESSED_LOGS.equals("YES")) { 
+                  // Delete the CDN log ZIP file
+                  s3Client.deleteObject(bucketName, key);
+                  System.out.println("============ Deleted Zip " + zips + " ============"); 
+                  deletedZips++;
+            }
+         } catch (IOException e) {
+			
+               e.printStackTrace();
+               return eventsNumber;
+		     } finally {
+				     if (br != null) {
+                br.close();
+             }
+   
+             if (eventsNumber >= n) { 
+                System.out.println("\n>>> " + eventsNumber + " events in " + zips + " Zip files. Deleted " + deletedZips + " Zip files.\n");
+                return eventsNumber;
+            }
+         }
+                
+        } // for (continue to next ZIP file
+        
+        // If there are more ZIP files, read next batch of 1000
+        if (objectListing.isTruncated()) {
+            nextObjectListing = s3Client.listNextBatchOfObjects(objectListing);
+            objectListing = nextObjectListing;
+        } else 
+            more = false; // no more files
+        
+       } // while next objectListing
+        
+       System.out.println("\n>>> " + eventsNumber + " events in " + zips + " Zip files. Deleted " + deletedZips + " Zip files.\n");
+       return eventsNumber;
+    }
+}
---
+++ src/main/java/dashboard/Mixpanel.java
@@ -0,0 +1,108 @@
+/*
+ * Copyright (c) 2013 Dror. All rights reserved
+ * <p/>
+ * The software source code is proprietary and confidential information of Dror.
+ * You may use the software source code solely under the terms and limitations of
+ * the license agreement granted to you by Dror.
+ */
+
+package dashboard;
+
+import java.io.IOException;
+import java.util.Date;
+
+// For date conversion to UNIX epoch format
+import java.text.SimpleDateFormat;
+
+import org.json.JSONObject;
+
+import org.apache.commons.codec.binary.Base64;
+import org.apache.commons.codec.binary.StringUtils;
+
+// For the HTTP POST
+import org.apache.commons.httpclient.*;
+import org.apache.commons.httpclient.methods.*;
+
+// Mixpanel
+//import com.mixpanel.mixpanelapi.ClientDelivery;
+//import com.mixpanel.mixpanelapi.MessageBuilder;
+//import com.mixpanel.mixpanelapi.MixpanelAPI;
+
+public class Mixpanel
+{
+
+   // =============================
+   // Post CDN event to Mixpanel
+   //
+   // =============================
+   public int postCDNEventToMixpanel(String API_KEY, String TOKEN, 
+                                     String ip, String eventName, String eventTime, String method,
+                                      String fileName, String fName, String userAgent, 
+                                      String statusCode, String registrant) throws IOException 
+   {
+  
+   try {
+      SimpleDateFormat sdf  = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+      Date date = sdf.parse( eventTime );
+      long timeInSecSinceEpoch = date.getTime();
+      if (timeInSecSinceEpoch > 0) timeInSecSinceEpoch = timeInSecSinceEpoch / 1000;   
+
+      
+      JSONObject obj1 = new JSONObject();
+      obj1.put("distinct_id", ip);
+      obj1.put("ip", ip);
+      obj1.put("File path", fileName);
+      obj1.put("File name", fName);
+      obj1.put("User agent", userAgent);
+      obj1.put("Status code", statusCode);
+      obj1.put("Method", method);
+      obj1.put("Registrant", registrant);
+      obj1.put("time", timeInSecSinceEpoch ); 
+      obj1.put("token", TOKEN);
+   
+      JSONObject obj2 = new JSONObject();
+      obj2.put("event", eventName);
+      obj2.put("properties", obj1);
+
+      String s2 = obj2.toString();
+      String encodedJSON = Base64.encodeBase64String(StringUtils.getBytesUtf8(s2));
+      
+      return postRequest("http://api.mixpanel.com/import", "data", encodedJSON, "api_key", API_KEY );
+     } catch (Exception e) {
+         //throw new RuntimeException("Can't POST to Mixpanel.", e);
+         e.printStackTrace();
+         return 0;
+     }
+   }
+  
+   public int postRequest(String url, String p1, String v1, String p2, String v2)
+   {
+    try {
+     String contents = ""; 
+     HttpClient client = new HttpClient();
+     PostMethod method = new PostMethod( url );
+
+	   // Configure the form parameters
+	   method.addParameter( p1, v1 );
+	   method.addParameter( p2, v2 );
+    
+     // Add more details in the POST response 
+	   method.addParameter( "verbose", "1" );
+
+	   // Execute the POST method
+ 
+     int statusCode = client.executeMethod( method );
+     contents = method.getResponseBodyAsString();
+     method.releaseConnection();
+     if (statusCode != 200 || contents.charAt(11) != '1') {  // Post to Mixpanel Failed
+        System.out.println("Mixpanel Post respone: " + Integer.toString(statusCode) + " - " + contents);
+        return 0;
+     }
+     return 1;
+    }
+    catch( Exception e ) {
+        e.printStackTrace();
+        return 0;
+    }
+  }
+}
